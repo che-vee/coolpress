5d491f17114f93a2f9b01af87a6e1544a6704636
Valentine <VEE.LANGAT@GMAIL.COM>
2022-11-13 17:16:27 +0100

    config admin site

--- coolpress/press/admin.py
+++ coolpress/press/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from press.models import Category, Post, CoolUser
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ['category']
+    list_display = ['title', 'author']
+
+
+admin.site.register(Post, PostAdmin)
+
+
+class CoolUserAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(CoolUser, CoolUserAdmin)
